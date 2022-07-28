a67dfac7292a7590edab9fa735dfb9d8bd4063ac
ManthanK1995 <99877287+ManthanK1995@users.noreply.github.com>
2022-07-28 18:48:41 +0530

    // forget password ui
    // forget password ui

--- tour_life/lib/view/forget_password/forget_password.dart
+++ tour_life/lib/view/forget_password/forget_password.dart
@@ -28,71 +28,74 @@ class _ForgetPassScreenState extends State<ForgetPassScreen> {
   }
 
   Widget buildMainView({Size? size}) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          Strings.appNameStr,
-          style: TextStyle(
-            color: Colorses.white,
-            fontSize: 40,
-            fontFamily: 'Inter-Regular',
+    return Container(
+      height: ,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            Strings.appNameStr,
+            style: TextStyle(
+              color: Colorses.white,
+              fontSize: 40,
+              fontFamily: 'Inter-Regular',
+            ),
           ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              children: [
-                Text(
-                  Strings.forgotPasswordStr,
-                  style: TextStyle(
-                      color: Colorses.white,
-                      fontSize: 35,
-                      fontFamily: 'Inter-Medium'),
-                ),
-                Text(
-                  "To reset your password, you need your email or",
-                  style: TextStyle(
-                      color: Colorses.white,
-                      fontSize: 14,
-                      fontFamily: 'Inter-Medium'),
-                ),
-                Text(
-                  "mobile number that can be authenticated",
-                  style: TextStyle(
-                      color: Colorses.white,
-                      fontSize: 14,
-                      fontFamily: 'Inter-Medium'),
-                ),
-              ],
-            )
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20),
-          child: Image.asset(Images.securityImage),
-        ),
-        buildEmailField(),
-        Row(
-          children: [
-            Column(
-              children: [
-                CommanBtn(
-                  text: "Reset Password",
-                  bgColor: Colorses.red,
-                  txtColor: Colorses.white,
-                ),
-                CommanBtn(
-                  text: "  Back to Login  ",
-                  bgColor: Colorses.grey,
-                  txtColor: Colorses.white,
-                ),
-              ],
-            )
-          ],
-        )
-      ],
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    Strings.forgotPasswordStr,
+                    style: TextStyle(
+                        color: Colorses.white,
+                        fontSize: 35,
+                        fontFamily: 'Inter-Medium'),
+                  ),
+                  Text(
+                    "To reset your password, you need your email or",
+                    style: TextStyle(
+                        color: Colorses.white,
+                        fontSize: 14,
+                        fontFamily: 'Inter-Medium'),
+                  ),
+                  Text(
+                    "mobile number that can be authenticated",
+                    style: TextStyle(
+                        color: Colorses.white,
+                        fontSize: 14,
+                        fontFamily: 'Inter-Medium'),
+                  ),
+                ],
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Image.asset(Images.securityImage),
+          ),
+          buildEmailField(),
+          Row(
+            children: [
+              Column(
+                children: [
+                  CommanBtn(
+                    text: "Reset Password",
+                    bgColor: Colorses.red,
+                    txtColor: Colorses.white,
+                  ),
+                  CommanBtn(
+                    text: "  Back to Login  ",
+                    bgColor: Colorses.grey,
+                    txtColor: Colorses.white,
+                  ),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 
